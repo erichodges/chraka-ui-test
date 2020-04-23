0dea31d263f805a0d76f63dcf946b7b933e5a7da
Eric Hodges <ehodges2004@gmail.com>
2020-04-23 16:11:41 -0700

    Feature: Added docs page and a docsLayout component

---
+++ src/components/docsLayout.js
@@ -0,0 +1,51 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import { ColorModeProvider, CSSReset, theme, ThemeProvider } from "@chakra-ui/core";
+import { css, Global } from '@emotion/core';
+import 'focus-visible/dist/focus-visible';
+import React from "react";
+
+const GlobalStyles = css`
+  /*
+    This will hide the focus indicator if the element receives focus    via the mouse,
+    but it will still show up on keyboard focus.
+  */
+  .js-focus-visible :focus:not([data-focus-visible-added]) {
+     outline: none;
+     box-shadow: none;
+   }
+`;
+
+
+
+
+const Layout = ({ children }) => {
+
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+      <CSSReset />
+      <Global styles={GlobalStyles} />
+      <ColorModeProvider>
+      
+      <div>
+        <main>{children}</main>
+        <footer>
+          <h3>docs footer</h3>
+        </footer>
+      </div>
+      </ColorModeProvider>
+      </ThemeProvider>
+    </>
+  )
+}
+
+
+
+export default Layout;
