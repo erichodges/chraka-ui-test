548091eec22abd248b69b10021cf0eb366ed05b9
Eric Hodges <ehodges2004@gmail.com>
2020-05-14 14:26:47 -0700

    Layout: cleaned up index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import { Box, Heading, Text, useColorMode } from "@chakra-ui/core"
+import { Box, useColorMode } from "@chakra-ui/core"
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -12,11 +12,6 @@ return (
     <SEO title="Home" />
     <Box bg={colorMode === "light"? "grey.600" : "grey.800"} h="94vh" w="100vw">    
     </Box>
-    <Heading as="h3" size="xl">Hi people</Heading>
-    <Text fontSize="xl">Welcome to your new Gatsby site.</Text>
-    <Text fontSize="xl">Now go build something great.</Text>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-    </div>
 
   </Layout>
 )
