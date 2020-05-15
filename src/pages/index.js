9352126ca7d4e3e11c6dfdeee45cbce47c2206cf
Eric Hodges <ehodges2004@gmail.com>
2020-05-15 14:40:29 -0700

    Layout: Updated heading layout of index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import { Box, useColorMode } from "@chakra-ui/core"
+import { Box, Heading, IconButton, useColorMode } from "@chakra-ui/core"
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -10,7 +10,26 @@ const IndexPage = (props) => {
 return (
   <Layout>
     <SEO title="Home" />
-    <Box bg={colorMode === "light"? "grey.600" : "grey.800"} h="94vh" w="100vw">    
+    <Box bg={colorMode === "light"? "grey.600" : "grey.800"} h="94vh" w="100vw" display="flex" flexDir="column" alignItems="center" justifyContent="center" color={colorMode === "light"? "black" : "#ccc"}>
+      <Box>
+        <Box display="flex" alignItems="center">
+        <IconButton
+          aria-label={`Switch to ${
+            colorMode === "light" ? "dark" : "light"
+          } mode`}
+          isRound="true"
+          variant="ghost"
+          color="current"
+          padding="0"
+          ml="-.3rem"
+          fontSize="36px"            
+          icon="lock"
+        />
+        <Heading as="h1" size="2xl" color='pri' style={{letterSpacing: '-4px'}}>useAuth</Heading>
+        </Box>
+        <Heading as="h2" size="lg" mb={2} >Authentication for React Apps</Heading>
+        <code>yarn add react-use-auth</code>
+      </Box>
     </Box>
 
   </Layout>
